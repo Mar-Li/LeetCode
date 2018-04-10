e7e5b857e0a6ad035331c105e1b62560b62ee592
Fengshuang Li <fengshuang1102@gmail.com>
2018-04-10 02:29:14 -0500

    clean up a little bit

--- Fengshuang/python/sum.py
+++
@@ -1,9 +0,0 @@
-cost = [
-    ('jongro bbq', 108.44),
-    ('ladym', 20.06),
-    ('shabu', 91.21),
-    ('lionking', 485.76),
-    ('sake bar hagi', 125.00),
-    ('red robster', 120.00)
-]
-print(sum([i[1] for i in cost])/2)
